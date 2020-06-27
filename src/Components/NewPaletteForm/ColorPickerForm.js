c70675efd7f941dd83b9bc809b9c21eb69643ca9
George Gardner <gpgardner@yahoo.com>
2020-06-27 15:54:07 -0400

    styling color picker

--- src/Components/NewPaletteForm/ColorPickerForm.js
+++ src/Components/NewPaletteForm/ColorPickerForm.js
@@ -2,8 +2,26 @@ import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import { ChromePicker } from "react-color";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { withStyles } from "@material-ui/core/styles";
 
 //TODO: FIX BUG WHERE COLOR SAYS ALREADY CHOOSEN
+
+const styles = {
+    picker: {
+        width:"100%!important",
+        marginTop: "2rem",
+    },
+    addColor: {
+        width: "100%",
+        padding: "1rem",
+        marginTop: "1rem",
+        fontSize: "2rem",
+    },
+    colorNameInput: {
+        width: "100%",
+        height: "70px",
+    },
+}
 
 class ColorPickerForm extends Component {
   constructor(props) {
@@ -49,19 +67,23 @@ class ColorPickerForm extends Component {
   }
 
   render() {
-    const { paletteIsFull } = this.props;
+    const { paletteIsFull,classes } = this.props;
     const { currentColor, newColorName } = this.state;
     return (
       <div>
         <ChromePicker
           color={currentColor}
           onChangeComplete={this.updateCurrentColor}
+          className={classes.picker}
         />
         <ValidatorForm onSubmit={this.addNewColor}>
           <TextValidator
             label="Color Name"
-            name="newColorName"
             value={newColorName}
+            className={classes.colorNameInput}
+            name="newColorName"
+            variant="filled"
+            margin="normal"
             onChange={this.handleTextChange}
             validators={["required", "isColorNameUnique", "isColorUnique"]}
             errorMessages={[
@@ -75,6 +97,7 @@ class ColorPickerForm extends Component {
             type="submit"
             color="primary"
             disabled={paletteIsFull}
+            className={classes.addColor}
             style={paletteIsFull ? {} : { backgroundColor: currentColor }}
           >
             {paletteIsFull ? "Palette Full" : "Add Color!"}
@@ -85,4 +108,4 @@ class ColorPickerForm extends Component {
   }
 }
 
-export default ColorPickerForm;
+export default withStyles (styles)(ColorPickerForm);
--- src/Components/NewPaletteForm/NewPaletteForm.js
+++ src/Components/NewPaletteForm/NewPaletteForm.js
@@ -13,7 +13,6 @@ import { arrayMove } from "react-sortable-hoc";
 import PaletteFormNav from "./PaletteFormNav";
 import ColorPickerForm from "./ColorPickerForm";
 
-
 //TODO: put drawerwidth in a variables file
 const drawerWidth = 400;
 
@@ -31,6 +30,8 @@ const styles = (theme) => ({
   },
   drawerPaper: {
     width: drawerWidth,
+    display: "flex",
+    alignItems: "center"
   },
   drawerHeader: {
     display: "flex",
@@ -55,6 +56,20 @@ const styles = (theme) => ({
       duration: theme.transitions.duration.enteringScreen,
     }),
     marginLeft: 0,
+  },
+  container: {
+    width: "90%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttons: {
+    width: "100%",
+  },
+  button: {
+    width: "50%",
   },
 });
 
@@ -164,29 +179,33 @@ class NewPaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
-          <Typography variant="h4">Design your palette</Typography>
-          <div>
-            <Button
-              variant="contained"
-              color="primary"
-              disabled={paletteIsFull}
-              onClick={this.addRandomColor}
-            >
-              {paletteIsFull ? "Palette Full" : "Random Color"}
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={this.clearPalette}
-            >
-              Clear Palette
-            </Button>
+          <div className={classes.container}>
+            <Typography variant="h4" gutterBottom>Design your palette</Typography>
+            <div className={classes.buttons}>
+              <Button
+                variant="contained"
+                color="primary"
+                disabled={paletteIsFull}
+                onClick={this.addRandomColor}
+                className={classes.button}
+              >
+                {paletteIsFull ? "Palette Full" : "Random Color"}
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={this.clearPalette}
+                className={classes.button}
+              >
+                Clear Palette
+              </Button>
+            </div>
+            <ColorPickerForm
+              paletteIsFull={paletteIsFull}
+              addNewColor={this.addNewColor}
+              colors={colors}
+            />
           </div>
-          <ColorPickerForm
-            paletteIsFull={paletteIsFull}
-            addNewColor={this.addNewColor}
-            colors={colors}
-          />
         </Drawer>
         <main
           className={classNames(classes.content, {
